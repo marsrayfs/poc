5903f568403bf64d5bfb77c6fd954dbdd2dddb10
jayrek-fst <tabasa.jayrek@fullspeedtechnologies.com>
2023-01-19 18:24:39 +0800

    -implement file upload and update user information screen

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:standalone_pkg/domain/repository/firebase_user_repository_impl.dart';
@@ -22,7 +23,6 @@ class DashBoardScreen extends StatelessWidget {
         },
         builder: (context, state) {
           if (state is UserInfoLoaded) {
-            // final user = ((state.user as Success)).data as User;
             return Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -34,12 +34,44 @@ class DashBoardScreen extends StatelessWidget {
                   const SizedBox(
                     height: 20,
                   ),
+                  CircleAvatar(
+                      backgroundColor: Colors.blue,
+                      radius: 80,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(80),
+                          child: CachedNetworkImage(
+                              imageUrl: state.user.photo.toString(),
+                              placeholder: (context, url) =>
+                                  const CircularProgressIndicator(),
+                              errorWidget: (context, url, error) =>
+                                  const Icon(Icons.error),
+                              fit: BoxFit.cover,
+                              width: 300,
+                              height: 300))),
+                  const SizedBox(
+                    height: 20,
+                  ),
                   Text('${state.user.nickname}'),
                   Text(
                     '${state.user.firstname} ${state.user.lastname}',
                     style: const TextStyle(
                         fontStyle: FontStyle.italic, fontSize: 20),
                   ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  _infoWidget('Birth Date: ',
+                      '${state.user.birthYear}-${state.user.birthDay}-${state.user.birthMonth}'),
+                  _infoWidget('Kana Name: ',
+                      '${state.user.firstnameKana} ${state.user.lastnameKana}'),
+                  _infoWidget('Gender: ', state.user.sex.toString()),
+                  _infoWidget('City: ', state.user.addressCity.toString()),
+                  _infoWidget(
+                      'Address Number: ', state.user.addressNumber.toString()),
+                  _infoWidget(
+                      'Prefecture: ', state.user.addressPrefecture.toString()),
+                  _infoWidget(
+                      'Phone Number: ', state.user.phoneNumber.toString()),
                 ],
               ),
             );
@@ -51,6 +83,32 @@ class DashBoardScreen extends StatelessWidget {
           }
           return Container();
         },
+      ),
+    );
+  }
+
+  _infoWidget(String title, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        children: [
+          const Spacer(),
+          Expanded(
+              flex: 2,
+              child: Text(
+                title,
+                textAlign: TextAlign.left,
+              )),
+          Expanded(
+            flex: 2,
+            child: Text(
+              value,
+              style: const TextStyle(fontSize: 18),
+              textAlign: TextAlign.left,
+            ),
+          ),
+          const Spacer()
+        ],
       ),
     );
   }
